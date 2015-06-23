50883f2407f12ee6c13c5fecf1ae5cc42f3e40d6
Raghavender Sahdev <raghavendersahdev@gmail.com>
2015-06-23 06:04:38 -0400

    Update and rename TimeLapseReg_.java to TimeLapseRegAlign_.java

--- TimeLapseRegAlign_.java
+++ TimeLapseRegAlign_.java
@@ -8,15 +8,12 @@ import java.awt.*;
 import java.io.*;
 import java.util.*;
 
-public class TimeLapseReg_ implements PlugIn 
+public class TimeLapseRegAlign_ implements PlugIn 
 {
-
 	public void run(String arg) 
 	{
-		//if (!showDialog())
-		//	return;
-		//ImageStack stack1 = imp1.getStack();
-		//ImageStack stack2 = imp2.getStack();
+		if (!showDialog())
+			return;
 		ImageStack stack3 = null;
 		try {
 			stack3 = transformThese();
@@ -24,17 +21,14 @@ public class TimeLapseReg_ implements PlugIn
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		//imp1.changes = false;
-		//imp1.close();
-		//imp2.changes = false;
-		//imp2.close();
 		new ImagePlus("Aligned Stacks", stack3).show();
-		IJ.register(TimeLapseReg_.class);
+		IJ.register(TimeLapseRegAlign_.class);
 	}
 	public ImageStack transformThese()throws IOException
 	{
 		String directry = "D:\\dataset\\";
 		String reference_img = "D:\\dataset\\img0000.tif";
+		String path2 = "C:\\Users\\Raghavender Sahdev\\Desktop\\BITS\\GSoC2015\\ImageJ ICNF\\"; //path where transformations file will be stored
 		ImagePlus ip = new ImagePlus(reference_img);
 		FileInfo infoF = ip.getFileInfo();
 		
@@ -47,8 +41,8 @@ public class TimeLapseReg_ implements PlugIn
 		
 		String mark1 = "0 "+height/2+" ";
 		String mark2 = width/2+" " + height/2 +" ";
-		String mark3 = width +" "+ height/2 + " ";
-		FileWriter transform = new FileWriter("C:\\Users\\Raghavender Sahdev\\Desktop\\BITS\\GSoC2015\\ImageJ ICNF\\transformations.csv");
+		String mark3 = width/2 +" "+ height + " ";
+		FileWriter transform = new FileWriter(path2 + "transformations.csv");
 		BufferedWriter br = new BufferedWriter(transform);
 		//br.write("hello worldddzz..!!");
 		
@@ -56,11 +50,11 @@ public class TimeLapseReg_ implements PlugIn
 				
 		ImageStack stack = new ImageStack(width, height);
 		File folder = new File(directry);
-		ArrayList<String> arr = listFilesForFolder(folder);
+		ArrayList<String> target = listFilesForFolder(folder);
 		
-		for(int i=0 ; i<arr.size()-250 ; i++)
+		for(int i=0 ; i<target.size() ; i++)
 		{
-			String myTurboRegOptions = "-align -file "+reference_img+" 0 0 "+(width-1)+" "+(height-1)+ " -file "+directry+arr.get(i)+ " 0 0 "+(width-1)+" "+(height-1)+ " -rigidBody " +mark1+mark1+mark2+mark2+mark3+mark3+"-hideOutput";
+			String myTurboRegOptions = "-align -file "+reference_img+" 0 0 "+(width-1)+" "+(height-1)+ " -file "+directry+target.get(i)+ " 0 0 "+(width-1)+" "+(height-1)+ " -rigidBody " +mark1+mark1+mark2+mark2+mark3+mark3+"-hideOutput";
 			//String myTurboRegOptions = "-transform -file D:\\dataset\\img000"+(i+1)+".tif 562 392 -rigidBody " +mark1+mark1+mark2+mark2+mark3+mark3+"-showOutput";
 			reg.run(myTurboRegOptions);
 
@@ -73,10 +67,12 @@ public class TimeLapseReg_ implements PlugIn
 			double tangle = Math.atan2(tpts[2][1] - tpts[1][1], tpts[2][0] - tpts[1][0]);
 			IJ.log(" Angle " + (180.0*(tangle-sangle)/Math.PI));
 			br.write((tpts[0][0] - spts[0][0]) + "," + (tpts[0][1] - spts[0][1]) + ","+(180.0*(tangle-sangle)/Math.PI));
+			// uncomment to store the points in the csv files
+			//br.write(tpts[0][0]+","+ tpts[0][1]) + "," + spts[0][0]+","+spts[0][1]);
 			br.newLine();
 			
 			
-			System.out.println("Hello: "+(i+1));
+			//System.out.println("Hello: "+(i+1));
 			ImagePlus imp = reg.getTransformedImage();
 			stack.addSlice("image", imp.getProcessor());
 		}
@@ -103,7 +99,14 @@ public class TimeLapseReg_ implements PlugIn
 	// following function not required yet will be used later to input the transformation settings
 	public boolean showDialog() 
 	{
-		// code for inputing various parameters goes here
+		
+		GenericDialog gd = new GenericDialog("ADDStack_");
+		gd.addTextAreas("Enter File Path here", "", 1, 1);
+		
+		
+		if (gd.wasCanceled())
+			return false;
+		
 		return true;
 	}
 	void error() 
